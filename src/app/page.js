c4a3a59101d3eeb5f5c9c9c328984462391ef14a
Daniel Sousa <daniel.sousa@theintentmedia.com>
2025-03-17 17:06:17 +0800

    .

--- src/app/page.js
+++ src/app/page.js
@@ -5,71 +5,11 @@ import ExplanationBlock from "@/components/ExplanationBlock"
 import { createDirectus, rest, readItems } from "@directus/sdk"
 
 export default async function Page({ params }) {
-  const client = createDirectus("http://0.0.0.0:8055/", {
-    fetchOptions: {
-      mode: "cors",
-    },
-  }).with(rest());
-
   
-    const landingPages = await client.request(readItems("landingPages", {
-      filter: {
-        slug: { _eq: params.slug }
-      }
-    })); 
-
-    console.log(params.slug)
-
-    console.log("query",landingPages)
-
-    const landingPage = landingPages[0]
 
   return (
     <div className="flex flex-col min-h-screen">
-      <Header logo={landingPage.logo}/>
-      <main className="flex-grow container mx-auto px-4 py-8 items-center flex">
-        <div className="grid md:grid-cols-2 gap-8">
-          <div className="space-y-6">
-            <h1 className="text-4xl font-bold text-primary">{landingPage.title}</h1>
-            <p className="text-lg text-muted-foreground">
-              {landingPage.description}
-            </p>
-            <div className="grid grid-cols-2 gap-6">
-              <ExplanationBlock
-                title="Step 1: Fill the Form"
-                className={" bg-muted-foreground/5"}
-                description="Provide your details in the form to get redirected to the Casino/SportsBook."
-              />
-              <ExplanationBlock
-                title="Step 2: Register"
-                className={" bg-muted-foreground/5"}
-                description="Create your account at the Casino/SportsBook to participate in the cashback offer."
-              />
-              <ExplanationBlock
-                title="Step 3: Deposit"
-                className={"bg-blue-radiant"}
-                description="Make your first deposit to start playing and qualify for the cashback."
-              />
-              <ExplanationBlock
-                title="Step 4: Play"
-                className={"bg-gold-radiant"}
-                description="Enjoy our wide range of games. Your play contributes to your cashback eligibility."
-              />
-              <div className="col-span-2">
-                <ExplanationBlock
-                  title="Step 5: Receive Cashback"
-                  className="bg-green-radiant"
-                  description="In 7 days, receive your cashback via PIX or MBWay."
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col justify-center">
-            <LeadForm />
-          </div>
-        </div>
-      </main>
-      <Footer />
+      ups
     </div>
   )
 }
